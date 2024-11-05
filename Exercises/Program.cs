c52a687565b2a507826ae2fae5679b1fab3668ca
Martin Tomka <martintomka@microsoft.com>
2024-11-05 16:07:14 +0100

    Fixes

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -1,14 +1,6 @@
 ﻿using Exercises;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Polly.Registry;
 
 var cancellationToken = CancellationToken.None;
 var files = Enumerable.Range(0, 50).Select(v => $"file{v}.txt").ToArray();
-var registry = new ResiliencePipelineRegistry<string>();
-var loggerFatory = LoggerFactory.Create(builder => builder.AddConsole().SetMinimumLevel(LogLevel.Error));
 
-IServiceCollection services = new ServiceCollection();
-Exercise7.ConfigureServices(services);
-var exercise = services.BuildServiceProvider().GetRequiredService<Exercise7>();
-await exercise.Run(files, cancellationToken);
+await new Exercise1().Run(files, cancellationToken);
